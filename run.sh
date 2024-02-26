e52854910e58daaaa67a3ff13b49e434341397a4
wasabrot <28702988+wasabrot@users.noreply.github.com>
2024-02-26 12:15:56 +0100

    bugfix

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-# Build for amd64
+# Build for amd64 - works
 docker build --platform linux/amd64 .
 
-# Build for arm64
+# Build for arm64 - breaks
+echo "============================================"
 docker build --platform linux/arm64 .
 
-# Build for arm64
-docker build --platform linux/arm64,linux/arm64 .
+# faster cross-build
+# docker build --platform linux/arm64,linux/arm64 .
